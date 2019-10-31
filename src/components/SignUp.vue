cf0146401de634f1bb5143b922cee7cb8bd953e8
rednotice <rednotice@users.noreply.github.com>
2019-10-31 16:24:20 +0100

    Vuetify added

---
+++ src/components/SignUp.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="signup">
+    <h2>Hallo was geht?!</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SignUp'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+      <sign-up></sign-up>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import SignUp from '@/components/SignUp.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    SignUp
   }
 }
 </script>
